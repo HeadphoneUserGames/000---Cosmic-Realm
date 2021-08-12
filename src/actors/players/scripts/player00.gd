e932b418499ff87a5c2e3170214c3eaf3e814639
HeadphoneUserDev <88811701+HeadphoneUserGames@users.noreply.github.com>
2021-08-12 15:24:51 +0800

    Click to Move Function
    Watched a tutorial about a click to move mechanics which I can use to rephrase based on what I want it for my game.

---
+++ src/actors/players/scripts/player00.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+var max_speed = 400
+var speed = 0
+var acceleration = 1200
+var move_direction # only for art animation
+var moving = false
+var destination = Vector2()
+var movement = Vector2()
+
+func _unhandled_input(event):
+	# Function that will take any unhandled input. 
+
+	# You can remove the if event.is_action_pressed("Click") for it is not needed
+	if event.is_action_pressed("click"):
+		moving = true
+		destination = get_global_mouse_position()
+
+
+func _physics_process(delta):
+	MovementLoop(delta)
+
+# around 4:11 of the video
+func MovementLoop(delta):
+	if moving == false:
+		speed = 0
+	else:
+		speed += acceleration * delta
+		if speed > max_speed:
+			speed = max_speed
+	movement = position.direction_to(destination) * speed
+	if position.distance_to(destination) > 5:
+		movement = move_and_slide(movement)
+	else:
+		moving = false
