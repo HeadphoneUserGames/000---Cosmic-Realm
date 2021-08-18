e2c44f9f5ca49dcfab05981446cdd2b2a950c334
HeadphoneUserDev <88811701+HeadphoneUserGames@users.noreply.github.com>
2021-08-18 23:34:42 +0800

    Organize to Different Scene
    Applying the same code to a new scene (player02) to clean stuff up.

--- src/actors/players/scripts/player02.gd
+++ src/actors/players/scripts/player02.gd
@@ -1,18 +1,46 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 1000
-const ACCELERATION = 50
-const FRICTION = 10
+const MAX_SPEED = 20000
+const ACCELERATION = 5000
+const FRICTION = 2000
+
+var max_speed = 20000
+var acceleration = 5000
+var friction = 2000
 
 var velocity = Vector2()
 var destination = Vector2()
 
-#----------------------------------------
+var moving = false
+var not_moving = true
+
+#--------------------------------------------------------
+
+func _ready():
+	
+	print(max_speed)
+	print(acceleration)
+	print(friction)
+	
+	pass
 
 func _unhandled_input(event):
 	
 	if event.is_action_pressed("click"):
 		destination = get_global_mouse_position()
+	
+	# Testing how to add things lol not that important
+	if event.is_action_pressed("ui_up"):
+		max_speed = max_speed * 2
+		acceleration = acceleration * 2
+		friction = friction * 2
+		
+		print("add stats")
+		print(max_speed)
+		print(acceleration)
+		print(friction)
+		
+		pass
 	
 	pass
 
@@ -24,7 +52,11 @@ func _process(delta):
 
 func movementloop(delta):
 	
-	velocity = position.direction_to(destination) * MAX_SPEED
+	if position.distance_to(destination) > 30:
+		velocity += position.direction_to(destination) * acceleration * delta
+		velocity = velocity.clamped(max_speed * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	
 	move_and_slide(velocity)
 	
